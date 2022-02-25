79067c3b745e50e512c091a3676c61e158d9259f
Washington Nieto <washington_nieto@hotmail.com>
2022-02-24 21:35:45 -0500

    The first commit

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Tienda de Camisetas</title>
+        <link rel="stylesheet" href="assets/css/styles.css">
+    </head>
+    <body>
+        <!-- CABECERA -->
+        <header id="header">
+            <div id="logo">
+                <img src="assets/img/camiseta.png" alt="Camiseta Logo" />
+                <a href="index.php">
+                    Tienda de camisetas
+                </a>
+            </div>
+            
+        </header>
+            
+        <!-- MENU -->
+        <nav id="menu">
+            <ul>
+                <li>
+                    <a href="#">Inicio</a>
+                </li>
+                <li>
+                    <a href="#">Categoria1</a>
+                </li>
+                <li>
+                    <a href="#">Categoria2</a>
+                </li>
+                <li>
+                    <a href="#">Categoria3</a>
+                </li>
+                <li>
+                    <a href="#">Categoria4</a>
+                </li>
+            </ul>
+        </nav>
+        
+        <!-- BARRA LATERAL -->
+        <!-- CONTENIDO CENTRAL -->
+        <!-- PIE DE PAGINA -->
+        
+    </body>
+</html>
+
