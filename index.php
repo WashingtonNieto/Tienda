f235f6d8bf607118ac695eba67c4d7324703662e
Wnieto <washington_nieto@hotmail.com>
2022-02-25 21:55:01 -0500

    otro

--- index.php
+++ index.php
@@ -60,34 +60,35 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edi
                             <li><a href="#">Mis Pedidos</a></li>
                             <li><a href="#">Gestionar Pedidos</a></li>
                             <li><a href="#">Gestionar Categorias</a></li>
-                            
+
                         </ul>
 
                     </div>
                 </aside>
 
-            </div>
-            <!-- CONTENIDO CENTRAL -->
-            <div id="central">
-                <div class="product">
-                    <img src="assets/img/camiseta.png" />
-                    <h2>Camiseta Azul Talla m</h2>
-                    <p> 130 Euros </p>
-                    <a href="">Comprar</a>
-                </div>
+                <!-- CONTENIDO CENTRAL -->
+                <div id="central">
+                    <h1>Productos destacados</h1>
+                    <div class="product">
+                        <img src="assets/img/camiseta.png" />
+                        <h2>Camiseta Azul Talla m</h2>
+                        <p> 130 Euros </p>
+                        <a href="" class="button">Comprar</a>
+                    </div>
 
-                <div class="product">
-                    <img src="assets/img/camiseta.png" />
-                    <h2>Pantalon Azul Talla m</h2>
-                    <p> 40 Euros </p>
-                    <a href="">Comprar</a>
-                </div>
+                    <div class="product">
+                        <img src="assets/img/camiseta.png" />
+                        <h2>Pantalon Azul Talla m</h2>
+                        <p> 40 Euros </p>
+                        <a href="" class="button">Comprar</a>
+                    </div>
 
-                <div class="product">
-                    <img src="assets/img/camiseta.png" />
-                    <h2>Blusa roja</h2>
-                    <p> 25 Euros </p>
-                    <a href="">Comprar</a>
+                    <div class="product">
+                        <img src="assets/img/camiseta.png" />
+                        <h2>Blusa roja</h2>
+                        <p> 25 Euros </p>
+                        <a href="" class="button">Comprar</a>
+                    </div>
                 </div>
             </div>
             <!-- PIE DE PAGINA -->
