c6a441fd0201718b2ac8147da2d2a2dd6b5bf92c
Wnieto <washington_nieto@hotmail.com>
2022-03-01 16:01:57 -0500

    Categorias

--- helpers/utils.php
+++ helpers/utils.php
@@ -6,8 +6,19 @@ class Utils{
             $_SESSION[$name] = null;
             unset($_SESSION[$name]);
         }
-        
         return $name;
+    }
+    
+    public static function isAdmin(){
+        if(!isset($_SESSION['admin'])){
+            header("Location:".base_url);
+        }else{
+            return true;
+        }
+    }
+    
+    public static function ShowCategorias(){
+        require_once 'models/';
     }
 }
 
