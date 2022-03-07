03fbeca3f1726f3b91836e7113981c1c9cf4b96e
Washington Nieto <washington_nieto@hotmail.com>
2022-03-06 20:42:27 -0500

    Carrito 1

---
+++ controllers/CarritoController.php
@@ -0,0 +1,64 @@
+<?php
+
+require_once 'models/producto.php';
+
+class CarritoController {
+
+    public function index() {
+        $carrito = $_SESSION['carrito'];
+        
+        require_once 'views/carrito/index.php';
+    }
+
+    public function add() {
+        if (isset($_GET['id'])) {
+            $producto_id = $_GET['id'];
+        } else {
+            if (!headers_sent()) {
+                header("Location:" . base_url . "categoria/index");
+            }
+        }
+        if (isset($_SESSION['carrito'])) {
+            $counter = 0;
+            foreach ($_SESSION['carrito'] as $indice => $elemento) {
+                if ($elemento['id_producto'] == $producto_id) {
+                    $_SESSION['carrito'][$indice]['unidades']++;
+                    $counter++;
+                }
+            }
+
+        }
+            
+        if (!isset($counter) || $counter == 0) {
+
+            //conseguir producto
+            $producto = new Producto();
+            $producto->setId($producto_id);
+            $producto = $producto->getOne();
+
+            if (is_object($producto)) {
+                $_SESSION['carrito'][] = array(
+                    "id_producto" => $producto->id,
+                    "precio" => $producto->precio,
+                    "unidades" => 1,
+                    "producto" => $producto
+                );
+            }
+        }
+        if (!headers_sent()) {
+            header("Location:" . base_url . "carrito/index");
+        }
+    }
+
+    public function remove() {
+        
+    }
+
+    public function delete_all() {
+        unset($_SESSION['carrito']);
+        if (!headers_sent()) {
+            header("Location:" . base_url . "carrito/index");
+        }
+    }
+
+}
--- views/layout/sidebar.php
+++ views/layout/sidebar.php
@@ -1,34 +1,43 @@
 <!-- BARRA LATERAL -->
 <aside id="lateral">
-    
+
+    <div id="login" class="block_aside">    
+        <h3>Mi carrito</h3>
+        <ul>
+            <li><a href="<?= base_url ?>carrito/index">Productos()</a></li>
+            <li><a href="<?= base_url ?>carrito/index">Total</a></li>
+            <li><a href="<?= base_url ?>carrito/gestion">Ver el carrito</a></li>
+        </ul>        
+    </div>
+
     <div id="login" class="block_aside">
-        
-        <?php if(!isset($_SESSION['identity'])): ?>
+
+        <?php if (!isset($_SESSION['identity'])): ?>
             <h3>Entrar a la web</h3>
-            <form action="<?=base_url?>usuario/login" method="post">
+            <form action="<?= base_url ?>usuario/login" method="post">
                 <label for="email">Email</label>
                 <input type="email" name="email" />
                 <label for="password">Contrasena</label>
                 <input type="password" name="password" />
                 <input type="submit" value="Enviar" />
             </form>
-            
+
         <?php else: ?>
-            <h3><?=$_SESSION['identity']->nombre?> <?=$_SESSION['identity']->apellidos?></h3>
+            <h3><?= $_SESSION['identity']->nombre ?> <?= $_SESSION['identity']->apellidos ?></h3>
         <?php endif; ?>
-            
+
         <ul>
-            <?php if(isset($_SESSION['admin'])): ?>
-                <li><a href="<?=base_url?>categoria/index">Gestionar Categorias</a></li>
-                <li><a href="<?=base_url?>producto/gestion">Gestionar Productos</a></li>
+            <?php if (isset($_SESSION['admin'])): ?>
+                <li><a href="<?= base_url ?>categoria/index">Gestionar Categorias</a></li>
+                <li><a href="<?= base_url ?>producto/gestion">Gestionar Productos</a></li>
                 <li><a href="#">Gestionar Pedidos</a></li>
             <?php endif; ?>
-                
-            <?php if(isset($_SESSION['identity'])): ?>
+
+            <?php if (isset($_SESSION['identity'])): ?>
                 <li><a href="#">Mis Pedidos</a></li>
-                <li><a href="<?=base_url?>usuario/logout">Cerrar sesión</a></li>
+                <li><a href="<?= base_url ?>usuario/logout">Cerrar sesión</a></li>
             <?php else: ?>
-                <li><a href="<?=base_url?>usuario/registro">Registrate aqui!</a></li>
+                <li><a href="<?= base_url ?>usuario/registro">Registrate aqui!</a></li>
             <?php endif; ?>
 
         </ul>
