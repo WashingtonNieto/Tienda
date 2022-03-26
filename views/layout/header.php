a26318516952347ccd5a1a4043a99d015e90b509
Washington Nieto <washington_nieto@hotmail.com>
2022-03-26 18:10:00 -0500

    Funciona Localmente

--- views/layout/header.php
+++ views/layout/header.php
@@ -6,7 +6,7 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edi
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>El Palacio De Mis Angelitos</title>
+        <title>Tienda de Camisetas</title>
         <link rel="stylesheet" href="<?=base_url?>assets/css/styles.css" />
     </head>
     <body>
@@ -14,9 +14,9 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edi
             <!-- CABECERA -->
             <header id="header">
                 <div id="logo">
-                    <img src="<?=base_url?>assets/img/angelito.png" alt="Angelito Logo" />
+                    <img src="<?=base_url?>assets/img/camiseta.png" alt="Camiseta Logo" />
                     <a href="<?=base_url?>">
-                        <h1>El Palacio De Mis Angelitos</h1>
+                        <h1>Tienda de Camisetas</h1>
                     </a>
                 </div>
 
